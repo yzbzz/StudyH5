ae2c49cce1382d4c9d9795c27d733a31b0278e2d
liuhongzhe <liuhongzhe@etcp.cn>
2018-09-21 18:02:57 +0800

    commit

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    devtool: 'eval-source-map',
+
+    entry: __dirname + "/app/main.js", //唯一入口
+    output:{
+        path: __dirname + "/public", //打包后的文件存放路径
+        filename: "bundle.js" //打包后输出文件的文件名
+    },
+
+    devServer: {
+        contentBase: "./public", // 本地服务器所加载的页面所在的目录
+        historyApiFallback: true, // 不跳转
+        inline: true // 实时刷新
+    }
+}
